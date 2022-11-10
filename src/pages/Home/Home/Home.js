fb0259752fad9d84b54837d7a5c460d8ac3b140c
sarkerjahiduzzaman <78559065+sarkerjahiduzzaman@users.noreply.github.com>
2022-11-10 23:18:36 +0600

    home full code

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -39,4 +39,30 @@ const SignUp = () => {
                             <input type="text" name='name' placeholder="name" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
-                            <label classNam
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <Link to='/login' className="label-text-alt link link-hover text-purple-700">Already have an account !!</Link>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary bg-rose-600" type="submit" value="Register" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
+
+
