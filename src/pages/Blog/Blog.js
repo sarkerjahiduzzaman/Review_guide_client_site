45e96cbbf7341eebcb24a7b9ee357552eb504cd9
sarkerjahiduzzaman <78559065+sarkerjahiduzzaman@users.noreply.github.com>
2022-11-10 23:14:06 +0600

    blog-5

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -18,3 +18,7 @@ const Blog = () => {
                         <h1 className="text-5xl font-bold">What is JWT, and how does it work??</h1>
                         
 
+                        <p className="py-6">JWT, or JSON Web Token, is an open standard used to share security information between two parties — a client and a server. Each JWT contains encoded JSON objects, including a set of claims. JWTs are signed using a cryptographic algorithm to ensure that the claims cannot be altered after the token is issued. <br />
+                           JWTs differ from other web tokens in that they contain a set of claims. Claims are used to transmit information between two parties. What these claims are depends on the use case at hand. For example, a claim may assert who issued the token, how long it is valid for, or what permissions the client has been granted.
+
+                            A JWT is a string made up of three parts, separated by dots (.), and serialized using base64. In the most common serialization format, compact serialization, the JWT looks something like this: xxxxx.yyyyy.zzzzz.
