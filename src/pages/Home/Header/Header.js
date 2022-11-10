6456926b0efb49ec4f2e826ce22a5a2d041dce92
sarkerjahiduzzaman <78559065+sarkerjahiduzzaman@users.noreply.github.com>
2022-11-10 23:16:15 +0600

    header 1st part

---
+++ src/pages/Home/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className=' pt-11 text-center'>
+            <div className="carousel w-full rounded-md">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src="https://i.pinimg.com/736x/15/76/0c/15760c78377cf4de5e19d1ae211f691f.jpg" alt='' className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src="https://tbbd-flight.s3.ap-southeast-1.amazonaws.com/blogmnBLpfLNc4d-ELwnUVJpjDpjvuyN1Ru-.jpg" alt='' className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right
