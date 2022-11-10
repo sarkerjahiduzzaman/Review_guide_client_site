7f2fdc0cbe95990a238ce0fd9c6e2a4894610c83
sarkerjahiduzzaman <78559065+sarkerjahiduzzaman@users.noreply.github.com>
2022-11-10 23:10:49 +0600

    blog code is add

---
+++ src/pages/Blog/Blog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="text-5xl font-bold">Difference between SQL and NoSQL ??</h1>
