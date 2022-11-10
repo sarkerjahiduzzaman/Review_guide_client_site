7ec8b7ab3d3c499d589c8ea12f575bc3107ce030
sarkerjahiduzzaman <78559065+sarkerjahiduzzaman@users.noreply.github.com>
2022-11-10 23:15:29 +0600

    blog all code is add

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -9,16 +9,43 @@ const Blog = () => {
                         <h1 className="text-5xl font-bold">Difference between SQL and NoSQL ??</h1>
                         <p className="py-6">SQL is the programming language used to interface with relational databases. (Relational databases model data as records in rows and tables with logical links between them). NoSQL is a class of DBMs that are non-relational and generally do not use SQL.</p>
 
-</div>
-</div>
-</div>
-<div className="hero min-h-screen bg-base-200">
+                    </div>
+                </div>
+            </div>
+            <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content text-center">
                     <div className="max-w-md">
                         <h1 className="text-5xl font-bold">What is JWT, and how does it work??</h1>
-                        
-
                         <p className="py-6">JWT, or JSON Web Token, is an open standard used to share security information between two parties — a client and a server. Each JWT contains encoded JSON objects, including a set of claims. JWTs are signed using a cryptographic algorithm to ensure that the claims cannot be altered after the token is issued. <br />
                            JWTs differ from other web tokens in that they contain a set of claims. Claims are used to transmit information between two parties. What these claims are depends on the use case at hand. For example, a claim may assert who issued the token, how long it is valid for, or what permissions the client has been granted.
 
                             A JWT is a string made up of three parts, separated by dots (.), and serialized using base64. In the most common serialization format, compact serialization, the JWT looks something like this: xxxxx.yyyyy.zzzzz.
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="text-5xl font-bold">What is the difference between javascript and NodeJS??</h1>
+                        <p className="py-6">JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed. Node. js, on the other hand, is an interpreter or execution environment for the JavaScript programming language.</p>
+
+                    </div>
+                </div>
+            </div>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="text-5xl font-bold">How does NodeJS handle multiple requests at the same time??</h1>
+                        <p className="py-6">NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept of event-driven architecture. NodeJS has its own EventLoop which is an infinite loop that receives requests and processes them. They handle 40K requests per second having Node.</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Blog;
+
